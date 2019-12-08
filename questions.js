6c194570964745a7281ebbb22a4868f47421be02
Zac Eskridge <zace118@gmail.com>
2019-12-08 14:20:33 -0700

    Added questions.js and jumbotron on HTML

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "What are the three main languages of web design?",
+        choices: ["Java, CSS, HTML", "JavaScript, CSS, HTML", "jQuery, CSS, HTML", "JavaScript, C++, HMLT"],
+        answer: "Javascript, CSS, HTML"
+    },
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Link", "High Tight Module Links", "Hyper Test Machine Learning", "Hype'T Muh Lawd"],
+        answer: "Hyper Text Markup Link"
+    },
+    {
+        title: "What symbol is used when using jQuery?",
+        choices: ["&", "&", "$", "!"],
+        answer: "$"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Cascade Style Sheet", "Cascading Style Sheet", "Cascading Sheet Style", "Cow Sharks Swim"],
+        answer: "Cascading Style Sheet"
+    },
+    {
+        title: "What's the correct syntax for creating a function?",
+        choices: ["function = myFunction()", "function myFunction", "fnctn(myFunction())", "function myFunction()"],
+        answer: "$"
+    },
+  ];
+  
